0b81715322a07cdd489be660a36e31f32e0c603f
dysss <dystoppai@gmail.com>
2024-10-11 10:21:36 +0800

    Change ports and urls to load from .env

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ const routeTable = require("./routes/routeTable.js");
 const app = express();
 
 const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:4173"],
+    origin: process.env.ALLOWED_ORIGINS.split(","),
     credentials: true,
     methods: "GET,POST,PUT",
     allowedHeaders: "Content-Type,Authorization",
